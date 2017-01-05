12ebf66ac21b98c500de96ba0dfa67092d4ff488
Frederick Lawler <fred@fredlawl.com>
2017-01-05 10:50:11 -0600

    mocked variable mapper

--- tests/SINSQLParserTest.php
+++ tests/SINSQLParserTest.php
@@ -1,16 +1,55 @@
 <?php
 
 use SINSQL\Exceptions\SINQLException;
+use SINSQL\Exceptions\TokenMismatchException;
+use SINSQL\Interfaces\IVariableMapper;
 use SINSQL\SINSQLParser;
 use SINSQL\StringBuffer;
 
 
 class SINSQLParserTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var IVariableMapper
+     */
+    private $variableMapper;
+    
+    private $mockVariable;
+    
+    public function SetUp()
+    {
+        $this->mockVariable = 'mockedVariable';
+        $this->variableMapper = \Mockery::mock(IVariableMapper::class)
+            ->shouldReceive('map')
+            ->andReturn($this->mockVariable)
+            ->mock();
+    }
+    
     public function testParseReturnsFalseOnEmptyInput()
     {
         $parser = new SINSQLParser(new StringBuffer(""));
         $this->assertFalse($parser->parse());
+    }
+    
+    public function testTokenMismatch()
+    {
+        try {
+            $parser = new SINSQLParser(new StringBuffer("::somevar"));
+            $parser->generateParseTree()->evaluate();
+            $this->assertTrue(false, "This test should've failed");
+        } catch (TokenMismatchException $e) {
+            $this->assertTrue(true);
+        }
+    }
+    
+    public function testGarbage()
+    {
+        $parser = new SINSQLParser(new StringBuffer("garbage"));
+//        try {
+        $parser->generateParseTree();
+//        } catch (SINQLException $e) {
+//            $this->expectException("Unable to construct parse tree.");
+//        }
     }
     
     public function testParse()
@@ -23,21 +62,9 @@ class SINSQLParserTest extends PHPUnit_Framework_TestCase
         $parser = new SINSQLParser(new StringBuffer($expected));
         $this->assertEquals($expected, $parser->generateParseTree()->evaluate());
         
-        $expected = "somevar";
-        $parser = new SINSQLParser(new StringBuffer(":" . $expected));
+        $expected = $this->mockVariable;
+        $parser = new SINSQLParser(new StringBuffer(":" . $expected), $this->variableMapper);
         $this->assertEquals($expected, $parser->generateParseTree()->evaluate());
-    
-        $expected = "somevar";
-        $parser = new SINSQLParser(new StringBuffer("::" . $expected));
-        $this->assertEquals($expected, $parser->generateParseTree()->evaluate());
-    
-        $parser = new SINSQLParser(new StringBuffer("garbage"));
-//        try {
-            $parser->generateParseTree();
-//        } catch (SINQLException $e) {
-//            $this->expectException("Unable to construct parse tree.");
-//        }
-        
     }
     
 }
